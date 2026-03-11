070541bd9dc48dd65f066e3245bccaed881e0447
Matias Alfonso <matias.alfonso@protezionecivile.fvg.it>
2026-03-11 16:49:31 +0100

    add material icons and Icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <span class="material-icons" :style="{ fontSize: size + 'px', color: color }">
+    {{ name }}
+  </span>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  size: { type: [String, Number], default: 24 },
+  color: { type: String, default: "currentColor" },
+});
+</script>
+
+<style scoped>
+.material-icons {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  user-select: none;
+}
+</style>
